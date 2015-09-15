17d270d41e12fae272d402d1f2cb7fcd3c122f08
cmcclatchey <cmcclatchey@e104-m-16.mchenry.edu>
2015-09-15 13:43:11 -0500

    Initial commit

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        int classNumber = Integer.parseInt("10000010", 2);
+        int roomNumber = Integer.parseInt("01101000", 2);
+
+
+        System.out.println("\nWe are in PRG-" + classNumber + "-001, room E" + roomNumber + ".");
+
+    }
+}
